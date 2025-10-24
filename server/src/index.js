dc1cbc94ee31ab65fa941d9ff22981e6c7c43fb7
Biswajit9609 <biswajitchatterjee347@gmail.com>
2025-10-24 12:24:22 +0530

    resolve site refresh issue

--- server/src/index.js
+++ server/src/index.js
@@ -25,6 +25,9 @@ const configRoutes = require('./routes/config');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Trust proxy for Vercel
+app.set('trust proxy', 1);
+
 // Security middleware
 app.use(helmet());
 app.use(cors({
